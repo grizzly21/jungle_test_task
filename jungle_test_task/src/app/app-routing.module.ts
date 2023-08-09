f851d347aaea9f2d9cfcd78a4983c460e3d7a7dd
grizzly21 <makintoch2104@gmail.com>
2023-08-09 15:12:03 +0300

    created user interface and routing

--- jungle_test_task/src/app/app-routing.module.ts
+++ jungle_test_task/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainPageComponent} from "./components/main-page/main-page.component";
+import {RepositoriesComponent} from "./components/repositories/repositories.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'repos', component: RepositoriesComponent},
+  {path: '**', redirectTo: '/', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- jungle_test_task/src/app/app.module.ts
+++ jungle_test_task/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
